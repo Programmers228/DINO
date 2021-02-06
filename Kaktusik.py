f865d0480aa167f429effb62de4f6c4305780ef6
HATSKER-228 <timakarvan@gmail.com>
2021-02-06 20:55:11 +0200

    optimisition and cheat

--- Kaktusik.py
+++ Kaktusik.py
@@ -3,7 +3,7 @@ import pygame
 image = pygame.image.load("kaktusik.png")
 
 class Kaktusik():
-    def __init__(self, width, height, pos, dino):
+    def __init__(self, width, height, pos, dino, cheat):
         self.image = image
         self.size = (20, 60)
         self.speed = [-1, 0]
@@ -12,6 +12,7 @@ class Kaktusik():
         self.pos = list(pos)
         self.dino = dino
         self.cheked = False
+        self.cheat = cheat
 
     def draw(self, display):
         display.blit(self.image,self.image.get_rect(topleft=self.pos))
@@ -22,7 +23,9 @@ class Kaktusik():
 
         if (self.dino.pos[0] >= self.pos[0] and self.dino.pos[0] <= self.pos[0] + self.size[0]) or (self.dino.pos[0] + self.dino.size[0] >= self.pos[0] and self.dino.pos[0] + self.dino.size[0] <= self.pos[0] + self.size[0]):
             if (self.dino.pos[1] >= self.pos[1] and self.dino.pos[1] <= self.pos[1] + self.size[1]) or (self.dino.pos[1] + self.dino.size[1] >= self.pos[1] and self.dino.pos[1] + self.dino.size[1] <=self.pos[1] + self.size[1]):
-                self.dino.die = True
+                if not self.cheat:
+                    self.dino.die = True
+
 
         if self.dino.pos[0] > self.pos[0] + self.size[0] and not self.cheked:
             self.cheked = True
--- main.py
+++ main.py
@@ -1,45 +1,66 @@
 import random
+import os
+import sys
 
+from Wrench import *
 from dinozavr import *
 from Kaktusik import *
 
 pygame.init()
+cheat = False
+if "Hacker228.txt" in os.listdir(sys.path[0]):
+    cheat = True
 
 music = pygame.mixer.music.load("Музон.mp3")
 pygame.mixer.music.play(-1)
-
+volume = 0.5
 width = 640
 height = 480
+FPS = 340
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("DINO")
 clock = pygame.time.Clock()
 game_end = False
 dino = Dino(width, height)
-cactus = [Kaktusik(width,height,(width-20,height-60), dino)]
+cactus = [Kaktusik(width,height,(width-20,height-60), dino, cheat)]
+randomizer = random.randint(300,340)
 font = pygame.font.Font('ALGER.TTF', 45)
 pause = False
-pause_image = pygame.image.load('PAUSED.png ')
+pause_image = pygame.image.load('PAUSED.png')
+gameover_image = pygame.image.load('GAME OVER.png')
 
 
 def spawn():
-    cactus.append(Kaktusik(width,height,(cactus[-1].pos[0]+random.randint(300,340),height-60), dino))
+    global randomizer
+    if cactus[-1].pos[0]+randomizer < width:
+        cactus.append(Kaktusik(width,height,(cactus[-1].pos[0]+randomizer,height-60), dino, cheat))
+        randomizer = random.randint(300, 340)
 while not game_end:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_end = True
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and not pause and not dino.die:
                 if not dino.jump:
                     dino.jump = 1
                     dino.speed[1] = -dino.jump_speed
                     print(111)
             if event.key == pygame.K_RETURN:
-                pause = not pause
+                pause = not pause and not dino.die
+            if event.key == pygame.K_KP_MINUS and volume > 0:
+                volume = cround(volume, 0.02, '-')
+                pygame.mixer.music.set_volume(volume)
+                print ( volume )
+            if event.key == pygame.K_KP_PLUS and volume < 1:
+                volume = cround(volume, 0.02, '+')
+                pygame.mixer.music.set_volume(volume)
+                print (volume )
+
 
 
     display.fill((0, 50, 255))
-    if not pause:
+    if not pause and not dino.die:
         dino.move()
     dino.draw(display)
     text = font.render(str(dino.score), True, (255,255,255))
@@ -49,19 +70,19 @@ while not game_end:
     a=[]
     for i in cactus:
         i.draw(display)
-        if not pause:
+        if not pause and not dino.die:
             i.move()
         if not (i.pos[0] < 0 or i.pos[0] > width):
             a.append(i)
     cactus = a
     if dino.die:
-        game_end = True
-    if not pause:
+        display.blit(gameover_image,gameover_image.get_rect(topleft=(width/2-144,height/2-39)))
+    if not pause and not dino.die:
         spawn()
     if pause:
         display.blit(pause_image,pause_image.get_rect(topleft=(width/2-144,height/2-39)))
     pygame.display.update()
-    clock.tick(340)
+    clock.tick(FPS)
 pygame.quit()
 
 quit()
